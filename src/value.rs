958180516be60e0d33369c7d9cefb70dc2b00151
zoryamba <zoryamba@gmail.com>
2023-10-12 21:18:55 +0300

    clone value

--- src/value.rs
+++ src/value.rs
@@ -1,21 +1,22 @@
 use indexmap::{IndexMap};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Number {
     Int(i64),
     Float(f64)
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Map<K: PartialEq + Eq + std::hash::Hash, V: PartialEq> {
     _map: MapImpl<K, V>,
 }
 
 type MapImpl<K, V> = IndexMap<K, V>;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Value {
     Null,
+    Undefined,
     Bool(bool),
     Number(Number),
     String(String),
